ea27fd5556d7e6bd29529a12e5be91ebee6dc49c
gabriel.neira <gabo.neira@gmail.com>
2020-06-21 22:32:12 -0500

    Implement about page

--- app/src/main/java/co/tuister/uisers/modules/main/AboutDialogFragment.kt
+++ app/src/main/java/co/tuister/uisers/modules/main/AboutDialogFragment.kt
@@ -1,10 +1,15 @@
 package co.tuister.uisers.modules.main
 
 import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDialog
 import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.databinding.DataBindingUtil
@@ -35,10 +40,78 @@ class AboutDialogFragment : AppCompatDialogFragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textViewFounder1.setOnClickListener {
+            openLinkedInPage(getString(R.string.about_founder_1_linked_in))
+        }
+
+        binding.textViewFounder2.setOnClickListener {
+            openLinkedInPage(getString(R.string.about_founder_2_linked_in))
+        }
+
+        binding.textViewEmail.setOnClickListener {
+            sendEmail(getString(R.string.about_email))
+        }
+
+        binding.textViewWhatsapp.setOnClickListener {
+            openWhatsapp(getString(R.string.about_whatsapp_number))
+        }
+
+        binding.textViewRepository.setOnClickListener {
+            openUrl(getString(R.string.about_repository_url))
+        }
+    }
+
     override fun show(manager: FragmentManager, tag: String?) {
         val ft = manager.beginTransaction()
         ft.add(this, tag)
         ft.commitAllowingStateLoss()
+    }
+
+    private fun openLinkedInPage(linkedId: String) {
+        var intent = Intent(Intent.ACTION_VIEW, Uri.parse("linkedin://add/%@$linkedId"))
+        val packageManager = requireContext().packageManager
+        val list =
+            packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
+        if (list.isEmpty()) {
+            intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.linkedin.com/in/$linkedId")
+            )
+        }
+        startActivity(intent)
+    }
+
+    private fun openWhatsapp(number: String) {
+        val url = "https://api.whatsapp.com/send?phone=$number"
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(url)
+        }
+        startActivity(intent)
+    }
+
+    private fun sendEmail(email: String) {
+        val i = Intent(Intent.ACTION_SEND)
+        i.type = "message/rfc822"
+        i.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."))
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(
+                requireContext(),
+                "There are no email clients installed.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun openUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(url)
+        }
+        startActivity(intent)
     }
 
     companion object {
