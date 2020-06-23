b428099c71bb6203adc6041e199432282cbed3f7
gabriel.neira <gabo.neira@gmail.com>
2020-06-23 07:15:00 -0500

    Create glide module

--- app/src/main/java/co/tuister/uisers/utils/extensions/ImageViewExtensions.kt
+++ app/src/main/java/co/tuister/uisers/utils/extensions/ImageViewExtensions.kt
@@ -4,17 +4,17 @@ import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.widget.ImageView
+import co.tuister.uisers.utils.view.GlideApp
 import com.androdocs.circleimagelibrary.CircleImageView
-import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 
 fun ImageView.setImageFromUrl(imageUrl: String) {
-    Glide.with(context).load(imageUrl).into(this)
+    GlideApp.with(context).load(imageUrl).into(this)
 }
 
 fun CircleImageView.setImageFromUri(uri: Uri?) {
-    Glide.with(context).asBitmap().load(uri).into(object : CustomTarget<Bitmap>() {
+    GlideApp.with(context).asBitmap().load(uri).into(object : CustomTarget<Bitmap>() {
 
         override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
             setImageBitmap(resource)
