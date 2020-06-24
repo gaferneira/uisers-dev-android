a8adca1e03204578d36797eece2026f8be0a866e
Gabriel Neira <gabo.neira@gmail.com>
2020-06-23 19:55:17 -0500

    Fix issue related to material colors on api21- (#38)
    Fix issue related to material colors on api21-

--- app/src/main/java/co/tuister/uisers/utils/analytics/FirebaseAnalytics.kt
+++ app/src/main/java/co/tuister/uisers/utils/analytics/FirebaseAnalytics.kt
@@ -2,6 +2,7 @@ package co.tuister.uisers.utils.analytics
 
 import android.app.Activity
 import android.content.Context
+import co.tuister.uisers.BuildConfig
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.logEvent
 import java.util.Date
@@ -10,38 +11,42 @@ class FirebaseAnalytics(
     context: Context
 ) : Analytics {
 
-    private val instance = FirebaseAnalytics.getInstance(context)
+    private val instance by lazy {
+        if (BuildConfig.DEBUG) {
+            null
+        } else FirebaseAnalytics.getInstance(context)
+    }
 
     override fun trackScreenView(activity: Activity, screenName: String, screenClassName: String) {
-        instance.setCurrentScreen(activity, screenName, screenClassName)
+        instance?.setCurrentScreen(activity, screenName, screenClassName)
     }
 
     override fun trackUserLogin() {
-        instance.logEvent(Analytics.EVENT_LOGIN) {
+        instance?.logEvent(Analytics.EVENT_LOGIN) {
             param("date", getCurrentDate())
         }
     }
 
     override fun trackUserLogout() {
-        instance.logEvent(Analytics.EVENT_LOGOUT) {
+        instance?.logEvent(Analytics.EVENT_LOGOUT) {
             param("date", getCurrentDate())
         }
     }
 
     override fun trackUserSignUp() {
-        instance.logEvent(Analytics.EVENT_SIGN_UP) {
+        instance?.logEvent(Analytics.EVENT_SIGN_UP) {
             param("date", getCurrentDate())
         }
     }
 
     override fun trackForgotPassword() {
-        instance.logEvent(Analytics.EVENT_FORGOT_PASSWORD) {
+        instance?.logEvent(Analytics.EVENT_FORGOT_PASSWORD) {
             param("date", getCurrentDate())
         }
     }
 
     override fun trackEvent(eventName: String) {
-        instance.logEvent(eventName) {
+        instance?.logEvent(eventName) {
             param("date", getCurrentDate())
         }
     }
--- app/src/main/java/co/tuister/uisers/utils/extensions/ImageViewExtensions.kt
+++ app/src/main/java/co/tuister/uisers/utils/extensions/ImageViewExtensions.kt
@@ -7,13 +7,13 @@ import android.net.Uri
 import android.widget.ImageView
 import co.tuister.uisers.common.BaseActivity
 import co.tuister.uisers.modules.login.register.RegisterFragment
+import co.tuister.uisers.utils.view.GlideApp
 import com.androdocs.circleimagelibrary.CircleImageView
-import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 
 fun ImageView.setImageFromUrl(imageUrl: String) {
-    Glide.with(context).load(imageUrl).into(this)
+    GlideApp.with(context).load(imageUrl).into(this)
 }
 
 fun BaseActivity.launchImagePicker() {
@@ -27,7 +27,7 @@ fun BaseActivity.launchImagePicker() {
 }
 
 fun CircleImageView.setImageFromUri(uri: Uri?) {
-    Glide.with(context).asBitmap().load(uri).into(object : CustomTarget<Bitmap>() {
+    GlideApp.with(context).asBitmap().load(uri).into(object : CustomTarget<Bitmap>() {
 
         override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
             setImageBitmap(resource)
