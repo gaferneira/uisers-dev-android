67ffaeb4eab9503cce0761140d103b82cf9641a1
gabriel.neira <gabo.neira@gmail.com>
2020-07-07 10:58:23 -0500

    Remove unnecessary methods

--- app/src/test/java/co/tuister/uisers/TestsExtensions.kt
+++ app/src/test/java/co/tuister/uisers/TestsExtensions.kt
@@ -1,44 +1,7 @@
 package co.tuister.uisers
 
 import io.mockk.mockk
-import junit.framework.TestCase.assertEquals
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 inline fun <reified T : Any> relaxedMockk(block: T.() -> Unit = {}): T {
     return mockk(relaxed = true, relaxUnitFun = true, block = block)
-}
-
-fun <T> Flow<T>.test(scope: CoroutineScope): TestObserver<T> {
-    return TestObserver(scope, this)
-}
-class TestObserver<T>(
-    scope: CoroutineScope,
-    flow: Flow<T>
-) {
-    private val values = mutableListOf<T>()
-    private val job: Job = scope.launch {
-        flow.collect { values.add(it) }
-    }
-    fun assertNoValues(): TestObserver<T> {
-        assertEquals("", emptyList<T>(), this.values)
-        return this
-    }
-
-    fun assertValues(vararg values: T): TestObserver<T> {
-        assertEquals(values.toList(), this.values)
-        return this
-    }
-
-    fun assertSize(size: Int): TestObserver<T> {
-        assertEquals(size, values.size)
-        return this
-    }
-
-    fun finish() {
-        job.cancel()
-    }
 }
